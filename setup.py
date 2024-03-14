33872011fb0cdeb39bac2f3cf5d5ecd51f97ccca
Prasad <26869583+prasadjivane@users.noreply.github.com>
2024-03-14 20:14:24 +0530

    clean

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pymytest",
-    version="1.0",
+    version="0.7",
     packages=find_packages(),
     install_requires=[
         "requests",
